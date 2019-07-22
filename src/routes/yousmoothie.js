6d4f028de10d0574b0f87e126fec246aeb3ee013
Christian Reyes <cmreyesqla598@gmail.com>
2019-07-21 18:01:37 -0700

    yousmoothie page icons and generator page

--- src/routes/yousmoothie.js
+++ src/routes/yousmoothie.js
@@ -3,5 +3,8 @@ const router = express.Router();
 const yousmoothieController = require("../controllers/yousmoothieController.js");
 
 router.get("/yousmoothie", yousmoothieController.index);
+router.get("/yousmoothie/blueberry", yousmoothieController.blueberry);
+router.get("/yousmoothie/banana", yousmoothieController.banana);
+router.get("/yousmoothie/strawberry", yousmoothieController.strawberry);
 
 module.exports = router;
